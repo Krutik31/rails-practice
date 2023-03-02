c43b980ca5d7e9f4bf0026f88fe8c67135b524b9
Krutik Patel <krutik.p@simformsolutions.com>
2023-03-02 19:21:43 +0530

    refactor: TE7-T116 Active Record Basics
    Followed Naming conventions in controller and made changes in other files accordingly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/home/', to: "home#index"
-  resources :product, only: [:index, :show]
+  resources :products, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
