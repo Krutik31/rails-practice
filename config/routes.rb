f2300f54bf1900ad089f0db372c30af870b4121e
Krutik Patel <krutik.p@simformsolutions.com>
2023-03-02 18:49:26 +0530

    perf: TE7-T116 Active Record Basics
    Done some changes to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get '/product/', to: "product#index"
   get '/home/', to: "home#index"
-  get 'product/:id', to: "product#show"
+  resources :product, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
