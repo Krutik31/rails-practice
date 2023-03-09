d98642fbc46180ad9f368a602cfb6b9e911db990
Krutik Patel <krutik.p@simformsolutions.com>
2023-03-09 16:00:12 +0530

    refactor: TE7-T117 Active Record Migrations
    Remove custom validation & use predefined keywords given by rails.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,20 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :price, presence: true
-
-  validate :validate_repeatancy, if: :check_record?
-
-  private
-
-  def validate_repeatancy
-    self.errors.add(:price, "Data already exists.")
-  end
-
-  def check_record?
-    @author = Author.find(self.author_id)
-    (!(@author.books.where(name: self.name).where(price: self.price).empty?))
-  end
   
 end
