60aa085749d58ee077419af6c8642888531796af
Krutik Patel <krutik.p@simformsolutions.com>
2023-04-13 14:12:23 +0530

    TE7-T121 Advanced active record query interface
    Create Product CRUD with title, description, price, capacity, is_active(boolean), status( In stock, out of stock, Coming soon  ) ( Use enum for status )
    
    Add a default scope for is_active true for the product.
    
    Make 2 separate routes: one route to display all products ( where is_active is true or false ), and the second route display products with default scope
    
    Create a customer CURD with first_name, last_name, email, and phone_number ( email should be uniq )
    
    Create a Order CRUD with quantity, total_price ( product price * order quantity ), product_id, status( booked, canceled ), customer_id
    
    Add a drop-down of the customer on the order page and you can select the customer from there.
    
    The product show page should display the total count order of that particular product
    
    Display customer records on the order page
    
    Display the top 3 customers with the sum of quantity in orders by Product_ID ( title should be “most sales product by quantity” on root page)
    
    Display top 3 customers with the sum of prices in order by product_if ( title should be “most sales product by price” on root page )
    
    Display the top 5 customer who has maximum booked orders ( status booked ) ( title should be “most booked order of product” on root page )
    
    Display the top 5 customer who has maximum canceled orders ( status canceled) ( title should be “most canceled order of product” on root page )’
    
    On the root, page add a product, customer, and order listing page link and add display 9,10,11,12 point
    
    Add a filter by data on the order listing page
    
    Add search by product name on order listing page (Display filed on order listing page: Order Id, product name, quantity, total_price, status)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,37 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Author.all.each do |author|
-  5.times do |i|
-    author.books.find_or_create_by(name: "Book#{i + 10} by #{author.first_name}", price: 650)
-  end
+# Author.all.each do |author|
+#   5.times do |i|
+#     author.books.find_or_create_by(name: "Book#{i + 10} by #{author.first_name}", price: 650)
+#   end
+# end
+
+# users = %w[admin krutik dhyey nil]
+# lastname = %w[admin patel sapara patel]
+# phonenumber = %w[1111122222 3333344444 5555566666 7777788888]
+# category = %w[Music Celebration Film Fashion]
+
+# 4.times do |i|
+#   User.find_or_create_by(id: i + 1, username: users[i], password: '123')
+#   Profile.find_or_create_by(firstname: users[i], lastname: lastname[i], phonenumber: phonenumber[i], user_id: i + 1)
+#   Address.find_or_create_by(address_line1: 'address_line1', address_line2: 'address_line2', city: 'Ahmedabad',
+#                             pincode: '383001', user_id: i + 1)
+#   Category.find_or_create_by(category_name: category[i])
+# end
+
+10.times do |item|
+  Item.find_or_create_by(id: item + 1, title: "Product #{item}", description: "Product Description #{item}",
+                         price: item * 1000, capacity: item * 10, is_active: rand(0..1), product_status: rand(0..2))
 end
 
-users = %w[admin krutik dhyey nil]
-lastname = %w[admin patel sapara patel]
-phonenumber = %w[1111122222 3333344444 5555566666 7777788888]
-category = %w[Music Celebration Film Fashion]
+10.times do |cust|
+  Customer.find_or_create_by(id: cust + 1, first_name: "John#{cust}", last_name: "Doe#{cust}",
+                             email: "john#{cust}@gmail.com", phone_number: "940980050#{cust}")
+end
 
-4.times do |i|
-  User.find_or_create_by(id: i + 1, username: users[i], password: '123')
-  Profile.find_or_create_by(firstname: users[i], lastname: lastname[i], phonenumber: phonenumber[i], user_id: i + 1)
-  Address.find_or_create_by(address_line1: 'address_line1', address_line2: 'address_line2', city: 'Ahmedabad',
-                            pincode: '383001', user_id: i + 1)
-  Category.find_or_create_by(category_name: category[i])
+20.times do |ord|
+  itemid = Item.all.to_a.sample.id
+  Order.find_or_create_by(id: ord + 1, quantity: rand(1..25), order_status: %w[booked cancelled].sample,
+                          item_id: itemid, customer_id: rand(1..10))
 end
